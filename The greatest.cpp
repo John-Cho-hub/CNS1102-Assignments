db5f4162c5f63e5dec610d857e821f5e9f6870aa
John-Cho-hub <johnchoche237@gmail.com>
2025-11-26 12:11:00 +0100

    Add files via upload

---
+++ The greatest.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a,b,c;
+	cout<<"Enter a number"<<endl;
+	cin>>a;
+	cout<<"Enter a number"<<endl;
+	cin>>b;
+	cout<<"Enter a number"<<endl;
+	cin>>c;
+	if(a>b && a>c){
+      cout<<"a is the greatest"<<a;
+}
+
+	else{
+	
+		if(b>a && b>c){
+			
+		cout<<"b is the greatest"<<b;
+		}
+		else{
+		
+		if(c>a && c>b){
+	
+			cout<<c<<" is the greatest";
+		}
+		}
+	}
+		
+	}
+
+
